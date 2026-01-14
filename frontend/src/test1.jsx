8dfe90d3e074c5a88f9eb4ac483a956ebe4113c4
bluesky <bluesky@example.com>
2026-01-14 17:02:45 +0900

    로그인

---
+++ frontend/src/test1.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Test1() {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                fontSize: '2rem',
+                fontFamily: 'Arial, sans-serif'
+            }}
+        >
+            로그인 페이지입니다
+             
+        </div>
+    );
+}
+
+export default Test1;

7777dcd3883ac7b5fdcee295dc2b4f8e47fd6557
hanuooo <hwhhh111@naver.com>
2026-01-21 14:42:10 +0900

    브랜드 페이징 처리

--- backend/src/main/java/com/poperp/backend/service/BrandService.java
+++ backend/src/main/java/com/poperp/backend/service/BrandService.java
@@ -4,6 +4,8 @@ import com.poperp.backend.dto.BrandResponseDto;
 import com.poperp.backend.entity.Brands;
 import com.poperp.backend.repository.BrandRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,11 +16,12 @@ public class BrandService {
 
     private final BrandRepository brandRepository;
 
-    public List<BrandResponseDto> getBrands() {
-        return brandRepository.findAll()
-                .stream()
-                .map(BrandResponseDto::from)
-                .toList();
+    public Page<BrandResponseDto> getActiveBrands(int page) {
+        int size = 10;
+
+        return brandRepository.findByActiveTrue(
+                PageRequest.of(page, size)
+        ).map(BrandResponseDto::from);
     }
 
     public BrandResponseDto getBrand(Long id) {

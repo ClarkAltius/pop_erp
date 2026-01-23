9f9cf467eb74a738a4fdd3838ffba70779c660cd
ClarkAltius <ilhwana@gmail.com>
2026-01-23 09:56:25 +0900

    브랜드관리 전체조회 (#37)
    Co-authored-by: dwa <apfhstkxkd77@naver.com>

--- backend/src/main/java/com/poperp/backend/dto/BrandResponseDto.java
+++ backend/src/main/java/com/poperp/backend/dto/BrandResponseDto.java
@@ -1,5 +1,6 @@
 package com.poperp.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.poperp.backend.entity.Brands;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,8 @@ public class BrandResponseDto {
     private String name;
     private BigDecimal commissionRate;
     private String contactEmail;
+
+    @JsonProperty("isActive")
     private boolean isActive;
 
     public static BrandResponseDto from(Brands brand) {
